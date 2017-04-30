33512470d78c83c85b7c802a7e715d386e99bb79
Scott Ysebert <endoplasmic@gmail.com>
2017-04-30 15:13:45 -0700

    :bug: - Fixed bugs in the test… Oops.

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 const record = require('node-record-lpcm16');
-const speech = new GoogleSpeech({projectId: ''}); // your google project id
+const GoogleSpeech = require('./index');
+const speech = new GoogleSpeech({ projectId: '' }); // your google project id
 
 const mic = record.start({
   sampleRate: 16000,
@@ -10,4 +11,7 @@ mic.on('data', speech.write);
 speech.start();
 console.log('Say some stuff');
 speech.on('transcription-update', console.log);
-speech.on('transcription-complete', data => console.log('ALL DONE!', data));
+speech.on('transcription-complete', (data) =>  {
+  console.log('\n\nTest passed!', data);
+  process.exit();
+});
