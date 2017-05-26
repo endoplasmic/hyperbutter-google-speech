a213b408beed8a57efc648dfc2c858fbd92fef9f
Scott Ysebert <endoplasmic@gmail.com>
2017-05-25 21:10:59 -0700

    :bug: - It looks like sometimes you’ll get an array of results and other times you don’t (no idea why…). :sparkles: - It also automatically subscribes to any `audio-input` engine you may have (like a microphone) You *must* set your mic threshold to 0 so that the google servers can stop the audio correctly.

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 const EventEmitter = require('events');
 const Speech = require('@google-cloud/speech');
 const util = require('util');
+const path = require('path');
+const fs = require('fs');
 
 function GoogleSpeech(config, settings) {
   const speechClient = Speech({
@@ -19,45 +21,80 @@ function GoogleSpeech(config, settings) {
     },
     interimResults: true,
     singleUtterance: true,
-    verbose: true,
+    verbose: false,
   };
 
-  let recognizeStream = undefined;
+  let recognizeStream;
 
   this.init = () => {
     this.emit('subscribe', {
+      start: this.start,
+      write: this.write,
       'audio-input': {
-        start: this.start,
+        started: this.start,
         data: this.write,
       },
     });
   };
 
-  this.start = () => {
+  this.start = (callback) => {
+    // make sure we don't have another one going
+    if (recognizeStream) {
+      this.stop();
+      this.emit('abort');
+      return;
+    }
+
+    let isFinal = false;
+
     recognizeStream = speechClient.createRecognizeStream(request)
-      .on('error', error => this.emit('error', error))
+      .on('error', (error) => {
+        this.emit('error', error);
+        if (callback) callback(error);
+      })
       .on('data', (data) => {
         if (data && data.results && data.endpointerType === 'ENDPOINTER_EVENT_UNSPECIFIED') {
-          let isFinal = false;
           let transcription = '';
 
-          data.results.forEach((result) => {
-            isFinal = result.isFinal;
-            transcription += result.transcript;
-          });
-
-          this.emit(isFinal ? 'transcription-complete' : 'transcription-update', transcription);
-          if (isFinal) {
-            recognizeStream.end();
-            recognizeStream = undefined;
+          if (Array.isArray(data.results)) {
+            data.results.forEach((result) => {
+              isFinal = result.isFinal;
+              transcription += result.transcript;
+            });
+          } else {
+            transcription = data.results;
           }
+
+          if (isFinal) this.emit('transcription-complete', transcription);
+          else this.emit('transcription-update', transcription);
+
+          if (isFinal) {
+            this.stop();
+            this.emit('status', `Heard: ${transcription}`);
+          }
+
+          if (callback) callback(null, { transcription, isFinal });
+        } else if (data.endpointerType === 'END_OF_AUDIO') {
+          isFinal = true;
         }
       });
+
     this.emit('listening');
   };
 
   this.write = (data) => {
-    if (recognizeStream !== undefined) recognizeStream.write(data);
+    if (recognizeStream !== undefined) {
+      recognizeStream.write(data);
+    }
+  };
+
+  this.stop = () => {
+    this.emit('audio-input.stop');
+
+    if (recognizeStream !== undefined) {
+      recognizeStream.end();
+      recognizeStream = undefined;
+    }
   };
 
   return this;
